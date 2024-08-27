d0e164220537d77d36db15b8895744c6fc607b93
Roshan Binu Paul <92724577+ROSHAN0120@users.noreply.github.com>
2024-08-27 12:45:33 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,20 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
+
+  fuzzy_new fuzzy_new (
+    .clk(clk),
+    .rst_n(rst_n),
+        .rain_fall(uio_in[7:0]),     // 8-bit input for rainfall (0 to 100)
+        .soil_moisture(ui_in[7:0]), // 8-bit input for soil moisture (0 to 100)
+        .risk(uo_out[7:0])
+             );      
+
 
 endmodule
