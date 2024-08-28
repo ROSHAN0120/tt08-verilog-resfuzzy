8841072235a256792ec02cef65ffcca4a5f60161
Roshan Binu Paul <92724577+ROSHAN0120@users.noreply.github.com>
2024-08-28 08:54:49 +0530

    Update controller.v

--- src/controller.v
+++ src/controller.v
@@ -1,21 +1,19 @@
 
 module controller(
     input  clk,rst_n,
-    input wire [7:0] rain_fall,     // 8-bit input for rainfall (0 to 100)
-    input wire [7:0] soil_moisture, // 8-bit input for soil moisture (0 to 100)
+    input wire [7:0] data_bus,    // 8-bit input for soil moisture (0 to 100)
     input ss,
-    output ef,
-    output [7:0]risk
+    output  ef1,
+    output reg [7:0]s1,
+    output reg [7:0]s2
     );
     
     reg [7:0]r1;
     reg [7:0]r2;
     reg [3:0]count;
     reg efr;
-    wire [7:0]rain_w;
-    wire [7:0]soil_w;
-    
-    
+ 
+
     always @(posedge clk) begin    
         if(!rst_n)begin
             r1<=0;
@@ -26,41 +24,35 @@ module controller(
             
             case(ss)
                 1'b0:begin
-                     r1 <= rain_fall ;  
+                     r1 = data_bus ;  
                      count = count+4'b0001;
-                     $display("%0t\t%d\t%d", $time,r1,count);
+//                     $display("%0t\t r1 value :%d\t%d", $time,r1,count);
                      end
                 1'b1:begin
-                     r2 <= soil_moisture;
+                     r2 = data_bus;
                      count = count+4'b0001;
-                      $display("%0t\t%d\t%d", $time,r2,count);
+//                      $display("%0t\t r2 value :%d\t%d", $time,r2,count);
                      end
             endcase
             
             if(count == 4'b0010)begin
                 if (r1 !=0 && r2!=0)begin
-                    efr<=1;        
+                    efr=1;
+                    s1=r1;
+                    s2= r2;       
                 end
                   count =0; 
             end else begin
-                efr<=0;  
+                efr=0;  
                 count = count;          
             end
-        end      
+        end 
+//       $display("efr :\t%d",efr);  
+//       $display("s1 :\t%d",s1);  
+//       $display("s2 :\t%d",s2);     
     end
-   assign ef = efr;
-   assign rain_w = r1;
-   assign soil_w = r2;
-      
-   fuzzy fuzzy(
-    .clk(clk),
-    .rst_n(rst_n),
-    .ef(ef),
-    .rain(rain_w),
-    .soil(soil_w),
-    .risk(risk)           // 8-bit output risk (0 to 255)
-    );
-    
-    
+   assign ef1 = efr;
+//   assign s1 = r1;
+//   assign s2 = r2;
     
 endmodule
